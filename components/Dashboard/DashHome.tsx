fd8c39ee0a53ab3994c9eb4159e5f60f20841048
yasir5405 <yasirnaseem5415@gmail.com>
2026-06-15 01:03:17 +0530

    Added loading state while logging out

--- components/Dashboard/DashHome.tsx
+++ components/Dashboard/DashHome.tsx
@@ -2,25 +2,44 @@
 import { authClient } from "@/lib/auth-client";
 import { Button } from "../ui/button";
 import { Spinner } from "../ui/spinner";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
+import { useState } from "react";
 const DashHome = () => {
   const { data: session, isPending } = authClient.useSession();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [authError, setAuthError] = useState("");
+  const router = useRouter();
+
+  const signOut = async () => {
+    setIsLoading(true);
+    try {
+      const { error } = await authClient.signOut();
+
+      if (error) {
+        setAuthError(error.message ?? "");
+        return;
+      }
+
+      router.push("/login");
+    } catch {
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   if (isPending) {
     return <Spinner />;
   }
-
   return (
     <div className="flex flex-col gap-2">
       <h1>Welcome {session?.user.email}</h1>
-      <Button
-        onClick={async () => {
-          await authClient.signOut();
-          redirect("/login");
-        }}
-      >
+      <Button onClick={signOut} disabled={isLoading}>
+        {isLoading && <Spinner />}
         Logout
       </Button>
+
+      {authError && <p className="text-sm text-red-500">{authError}</p>}
     </div>
   );
 };
