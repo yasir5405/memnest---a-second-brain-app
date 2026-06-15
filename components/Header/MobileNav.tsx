f27ce33b118ee99a33b64726b99a516bcc3bad56
yasir5405 <yasirnaseem5415@gmail.com>
2026-06-16 02:43:48 +0530

    Add mobile nav + accordion

--- components/Header/MobileNav.tsx
+++ components/Header/MobileNav.tsx
@@ -6,6 +6,14 @@ import { HiOutlineBars2 } from "react-icons/hi2";
 import { RxCross2 } from "react-icons/rx";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { AnimatePresence, motion } from "motion/react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+
 const MobileNav = () => {
   const [open, setOpen] = useState(false);
 
@@ -15,11 +23,35 @@ const MobileNav = () => {
 
   return (
     <div className="md:hidden block relative">
-      {open ? (
-        <RxCross2 size={25} onClick={() => setOpen((prev) => !prev)} />
-      ) : (
-        <HiOutlineBars2 size={25} onClick={() => setOpen((prev) => !prev)} />
-      )}
+      <AnimatePresence mode="wait">
+        {open ? (
+          <motion.div
+            key="close"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.75 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+          >
+            <RxCross2 size={22} onClick={() => setOpen((prev) => !prev)} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="menu"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.75 }}
+            transition={{
+              duration: 0.2,
+              ease: "easeInOut",
+            }}
+          >
+            <HiOutlineBars2
+              size={25}
+              onClick={() => setOpen((prev) => !prev)}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       <div
         className={cn(
@@ -29,14 +61,44 @@ const MobileNav = () => {
             : "opacity-0 -translate-y-2 pointer-events-none",
         )}
       >
-        {mobileNavLinks.map((link) => (
-          <h1 key={link.name} className="font-semibold text-lg ">
-            {link.name}
-          </h1>
-        ))}
+        <Accordion
+          type="single"
+          collapsible
+          defaultValue=""
+          className="w-full gap-3.5"
+        >
+          {mobileNavLinks.map(({ name, subCategories }) => (
+            <AccordionItem value={name} key={name}>
+              <AccordionTrigger className="font-semibold text-lg data-open:text-primary">
+                {name}
+              </AccordionTrigger>
+              <AccordionContent className="py-4 overflow-visible">
+                <div className="flex flex-col pl-2 gap-2">
+                  {subCategories?.map(({ href, name, icon: Icon }) => (
+                    <Link
+                      href={href}
+                      key={name}
+                      className="text-lg flex items-center gap-2"
+                    >
+                      <Icon className="size-4" strokeWidth={1.7}/>
+                      {name}
+                    </Link>
+                  ))}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+
+        <Link href={"/customers"} className="font-semibold text-lg">
+          Customers
+        </Link>
+        <Link href={"/pricing"} className="font-semibold text-lg">
+          Pricing
+        </Link>
 
         <Button
-          variant={"secondary"}
+          variant={"outline"}
           asChild
           className="px-4 py-4 text-sm font-normal"
         >
--- lib/constants/constants.ts
+++ lib/constants/constants.ts
@@ -1,10 +1,23 @@
+import { Folders, History, Inbox, LucideIcon, NotebookPen, Search, Sparkles, SquarePen, WandSparkles } from "lucide-react";
+
 type MobileNavLinksType = {
   name: string;
+  subCategories?: {
+    name: string;
+    href: string;
+    icon: LucideIcon;
+  }[];
 };
 export const mobileNavLinks: MobileNavLinksType[] = [
-  { name: "Product" },
+  {
+    name: "Product",
+    subCategories: [
+      { name: "Capture", href: "/capture", icon: SquarePen },
+      { name: "Organise", href: "/organise", icon: Folders },
+      { name: "Recall", href: "/recall", icon: History },
+      { name: "Create", href: "/create", icon: WandSparkles },
+    ],
+  },
   { name: "Solution" },
   { name: "Resources" },
-  { name: "Customers" },
-  { name: "Pricing" },
 ];
